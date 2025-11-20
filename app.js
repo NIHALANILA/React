143739c9304bcc640907b1184926bd25f75329b5
nihalanila <nihalashirin0@gmail.com>
2025-11-20 16:58:18 +0530

    Clean main branch and fix gitignore

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import "./style.css";
     #wishlist
     #cart
 * body
-   &searchbar
+   &banner
    &card container
     #card
 *footer
@@ -45,12 +45,57 @@ const Header = () => {
   );
 };
 
+const Card=({props})=>{
+  const bookData= props
+  return(
+    <div className="card">
+      <img src="https://dcbookstore.com/uploads/product/images/3318505214542-Qabar-Final.jpg" className="card-img"  alt=""/>
+      <h4>{Title}</h4>
+      <h4>{Author}</h4>
+      <h4>{Price}</h4>
+      <button className="card-button">View Details</button>
+    </div>
+  
+  )
+}
+
+const bookObj={
+  data: {
+    books: [
+      {
+        id: "b001",
+        title: "Manju",
+        author: "M.T. Vasudevan Nair",
+        coverImage:
+          "https://dcbookstore.com/uploads/product/images/3346504213727-Manju-Final.jpg",
+        price: 90,
+        rating: 4.5,
+      }]}}
+
+const Body=()=>{
+  return(
+    <div className="body">
+      <div className="banner">
+        <img src="https://dcbookstore.com/uploads/homeslider/images/4864107377154-Rush-Hour-Banner.jpg"/>
+      </div>
+      <div className="cardcontainer">
+        <Card Title="manju" Author="MT" Price="90"/>
+        <Card Title="khasakinte" Author="ov" Price="900"/>
+        <Card Title="manju" Author="MT" Price="90"/>
+        
+        
+      </div>
+      
+    </div>
+  )
+}
 
 const Shoplayout=()=>{
   
   return(
   <div className="shopMain">
     {Header()}
+    {Body()}
   </div>)
 }
 const root=ReactDOM.createRoot(document.getElementById('root'))
